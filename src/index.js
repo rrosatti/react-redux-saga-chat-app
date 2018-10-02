a9ce5609db2b67d582af8c190d58911147f2de20
Rodrigo R. Galvao <rodrigorosattig@gmail.com>
2018-10-02 09:53:38 -0300

    Start implementing the "server side" code
    - A new folder called 'server' was created, which will contain all
    the information related to the server side of the application.
    - index.js contains the code related with the WebSocket.
    
    Signed-off-by: Rodrigo R. Galvao <rodrigorosattig@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import chat from './reducers';
+import { addUser } from './actions';
 
 const store = createStore(chat);
+
+store.dispatch(addUser('Me'));
 
 ReactDOM.render(
   <Provider store={store}>
